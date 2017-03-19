d3d195217ed7bae4e76a24b0cde0c111b90de0cc
Blik <rblick@bk.ru>
2017-03-19 22:39:14 +0200

    first_commit

---
+++ src/main/java/isr/naya/admiralproj/Application.java
@@ -0,0 +1,10 @@
+package isr.naya.admiralproj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Blik on 03/19/2017.
+ */
+@SpringBootApplication
+public class Application {
+}
