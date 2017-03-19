3e10e7190aa4e8715bb397ebce461c73c9febe43
Blik <rblick@bk.ru>
2017-03-20 01:19:21 +0200

    postgres_properties

--- src/main/java/isr/naya/admiralproj/Application.java
+++ src/main/java/isr/naya/admiralproj/Application.java
@@ -1,5 +1,6 @@
 package isr.naya.admiralproj;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -7,4 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
