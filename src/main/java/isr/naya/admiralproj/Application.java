16666d01e61a61b485ef903ce6cb2cd87d90f811
Blik <rblick@bk.ru>
2017-03-24 19:05:42 +0300

    jackson+hibernate

--- src/main/java/isr/naya/admiralproj/Application.java
+++ src/main/java/isr/naya/admiralproj/Application.java
@@ -1,11 +1,22 @@
 package isr.naya.admiralproj;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import static isr.naya.admiralproj.util.JsonUtil.getMapper;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Primary
+    @Bean
+    public ObjectMapper objectMapper() {
+        return getMapper();
     }
 }
---
+++ src/main/java/isr/naya/admiralproj/util/JsonUtil.java
@@ -0,0 +1,30 @@
+package isr.naya.admiralproj.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.ALL;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
+import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+
+public class JsonUtil {
+    public static ObjectMapper getMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Hibernate5Module());
+        mapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+
+        mapper.setVisibility(ALL, NONE);
+        mapper.setVisibility(FIELD, ANY);
+        mapper.setSerializationInclusion(NON_EMPTY);
+        mapper.configure(INDENT_OUTPUT, true);
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return mapper;
+    }
+}
