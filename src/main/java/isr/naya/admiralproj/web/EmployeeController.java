7b8de4d23ba1964fc17f218eafe79b38c5c3501f
Blik <rblick@bk.ru>
2017-03-29 18:44:25 +0300

    first_method

--- src/main/java/isr/naya/admiralproj/web/EmployeeController.java
+++ src/main/java/isr/naya/admiralproj/web/EmployeeController.java
@@ -1,9 +1,19 @@
 package isr.naya.admiralproj.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import isr.naya.admiralproj.AuthorizedUser;
+import isr.naya.admiralproj.model.Employee;
+import isr.naya.admiralproj.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/admin")
 public class EmployeeController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping()
+    public Employee get() {
+        return employeeService.get(AuthorizedUser.id());
+    }
 }
