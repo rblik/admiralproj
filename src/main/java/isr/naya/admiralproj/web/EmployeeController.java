f10b15b18f7d2ceed3e679cea9614089697319b0
Blik <rblick@bk.ru>
2017-03-29 18:45:13 +0300

    first_method

--- src/main/java/isr/naya/admiralproj/web/EmployeeController.java
+++ src/main/java/isr/naya/admiralproj/web/EmployeeController.java
@@ -12,7 +12,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping()
+    @GetMapping("/profile")
     public Employee get() {
         return employeeService.get(AuthorizedUser.id());
     }
