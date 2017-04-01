f7786df5de52101176a29df9c4f572a1443ffb96
mduhovniy <maxim.duhovniy@gmail.com>
2017-04-02 02:26:08 +0300

    new configurations

--- src/main/java/isr/naya/admiralproj/Application.java
+++ src/main/java/isr/naya/admiralproj/Application.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 
 import static isr.naya.admiralproj.util.JsonUtil.getMapper;
 
 @SpringBootApplication
+@Import({SecurityConfig.class, CorsConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/isr/naya/admiralproj/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package isr.naya.admiralproj;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
