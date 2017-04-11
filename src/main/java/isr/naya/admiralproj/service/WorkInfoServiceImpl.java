61bb501174bc4c66e837ecb47215e6737e0918d0
Blik <rblick@bk.ru>
2017-04-11 21:33:48 +0300

    fixing_service_routing

--- src/main/java/isr/naya/admiralproj/service/WorkInfoServiceImpl.java
+++ src/main/java/isr/naya/admiralproj/service/WorkInfoServiceImpl.java
@@ -36,30 +36,26 @@ public class WorkInfoServiceImpl implements WorkInfoService {
                                               Optional<Integer> employeeId,
                                               Optional<Integer> departmentId) {
 
-        List<WorkInfo> workInfos;
         if (employeeId.isPresent()) {
-            workInfos = workUnitRepository.getAllPartialBetweenDatesByEmployeeId(from, to, maxHours, employeeId.get());
+            return workUnitRepository.getAllPartialBetweenDatesByEmployeeId(from, to, maxHours, employeeId.get());
         } else if (departmentId.isPresent()) {
-            workInfos = workUnitRepository.getAllPartialBetweenDatesByDepartmentId(from, to, maxHours, departmentId.get());
+            return workUnitRepository.getAllPartialBetweenDatesByDepartmentId(from, to, maxHours, departmentId.get());
         } else {
-            workInfos = workUnitRepository.getAllPartialBetweenDates(from, to, maxHours);
+            return workUnitRepository.getAllPartialBetweenDates(from, to, maxHours);
         }
-        return workInfos;
     }
 
     @Override
     public List<WorkInfo> getMissingWorkInfos(LocalDate from, LocalDate to,
                                               Optional<Integer> employeeId,
                                               Optional<Integer> departmentId) {
-        List<WorkInfo> workInfos;
         if (employeeId.isPresent()) {
-            workInfos = generate(workUnitRepository.getAllNonEmptyDaysByDateBetweenAndEmployeeId(from, to, employeeId.get()), from, to, singletonList(employeeService.getWithDepartment(employeeId.get())));
+            return generate(workUnitRepository.getAllNonEmptyDaysByDateBetweenAndEmployeeId(from, to, employeeId.get()), from, to, singletonList(employeeService.getWithDepartment(employeeId.get())));
         } else if (departmentId.isPresent()) {
-            workInfos = generate(workUnitRepository.getAllNonEmptyDaysByDateBetweenAndDepartmentId(from, to, departmentId.get()), from, to, employeeService.getAllByDepartment(departmentId.get()));
+            return generate(workUnitRepository.getAllNonEmptyDaysByDateBetweenAndDepartmentId(from, to, departmentId.get()), from, to, employeeService.getAllByDepartment(departmentId.get()));
         } else {
-            workInfos = generate(workUnitRepository.getAllNonEmptyDaysBetweenDates(from, to), from, to, employeeService.getAllWithDepartments());
+            return generate(workUnitRepository.getAllNonEmptyDaysBetweenDates(from, to), from, to, employeeService.getAllWithDepartments());
         }
-        return workInfos;
     }
 
     public List<WorkInfo> getWorkInfos(LocalDate from, LocalDate to,
@@ -67,32 +63,23 @@ public class WorkInfoServiceImpl implements WorkInfoService {
                                        Optional<Integer> departmentId,
                                        Optional<Integer> projectId,
                                        Optional<Integer> clientId) {
-        List<WorkInfo> workInfos;
         if (employeeId.isPresent() && projectId.isPresent()) {
-            workInfos = workUnitRepository.getAllByDateBetweenAndEmployeeIdAndProjectId(from, to, employeeId.get(), projectId.get());
+            return workUnitRepository.getAllByDateBetweenAndEmployeeIdAndProjectId(from, to, employeeId.get(), projectId.get());
         } else if (employeeId.isPresent()) {
-            if (clientId.isPresent()) {
-                workInfos = workUnitRepository.getAllByDateBetweenAndEmployeeIdAndClientId(from, to, employeeId.get(), clientId.get());
-            } else {
-                workInfos = workUnitRepository.getAllByDateBetweenAndEmployeeId(from, to, employeeId.get());
-            }
+            return clientId.map(
+                    integer -> workUnitRepository.getAllByDateBetweenAndEmployeeIdAndClientId(from, to, employeeId.get(), integer)).
+                    orElseGet(() -> workUnitRepository.getAllByDateBetweenAndEmployeeId(from, to, employeeId.get()));
         } else if (projectId.isPresent()) {
-            if (departmentId.isPresent()) {
-                workInfos = workUnitRepository.getAllByDateBetweenAndDepartmentIdAndProjectId(from, to, departmentId.get(), projectId.get());
-            } else {
-                workInfos = workUnitRepository.getAllByDateBetweenAndProjectId(from, to, projectId.get());
-            }
+            return departmentId.map(
+                    integer -> workUnitRepository.getAllByDateBetweenAndDepartmentIdAndProjectId(from, to, integer, projectId.get())).
+                    orElseGet(() -> workUnitRepository.getAllByDateBetweenAndProjectId(from, to, projectId.get()));
         } else if (departmentId.isPresent()) {
-            if (clientId.isPresent()) {
-                workInfos = workUnitRepository.getAllByDateBetweenAndDepartmentIdAndClientId(from, to, departmentId.get(), clientId.get());
-            } else {
-                workInfos = workUnitRepository.getAllByDateBetweenAndDepartmentId(from, to, departmentId.get());
-            }
-        } else if (clientId.isPresent()) {
-            workInfos = workUnitRepository.getAllByDateBetweenAndClientId(from, to, clientId.get());
-        } else {
-            workInfos = workUnitRepository.getAllByDateBetween(from, to);
-        }
-        return workInfos;
+            return clientId.map(
+                    integer -> workUnitRepository.getAllByDateBetweenAndDepartmentIdAndClientId(from, to, departmentId.get(), integer)).
+                    orElseGet(() -> workUnitRepository.getAllByDateBetweenAndDepartmentId(from, to, departmentId.get()));
+        } else
+            return clientId.map(
+                    integer -> workUnitRepository.getAllByDateBetweenAndClientId(from, to, integer)).
+                    orElseGet(() -> workUnitRepository.getAllByDateBetween(from, to));
     }
 }
