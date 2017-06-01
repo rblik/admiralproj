ff926b73c79c6c4e507212edb4519f55f30aa53c
Blik <rblick@bk.ru>
2017-06-02 01:12:02 +0300

    small refactor

--- src/main/java/isr/naya/admiralproj/Application.java
+++ src/main/java/isr/naya/admiralproj/Application.java
@@ -3,6 +3,7 @@ package isr.naya.admiralproj;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -13,11 +14,16 @@ import static isr.naya.admiralproj.util.JsonUtil.getMapper;
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    /*@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }*/
 
     @Primary
     @Bean
